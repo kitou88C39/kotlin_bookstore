28b60f93be6028d8eab760eb41701ba7e4fcd718
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-02 20:46:14 +0900

    domain/Book.ktを作成する

--- src/main/kotlin/com/devtiro/bookstore/domain/book.kt
+++ src/main/kotlin/com/devtiro/bookstore/domain/book.kt
@@ -1,3 +1,8 @@
 package com.devtiro.bookstore.domain
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
 data class Book (var isbn: String, var title: String, var description: String, var image: String, var author: Author)
