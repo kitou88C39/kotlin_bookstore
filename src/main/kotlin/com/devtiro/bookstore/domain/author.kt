b2cc06a468e96cc3857140e2984396a0573c177f
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-02 20:04:15 +0900

    domain/Author.ktを作成する

--- src/main/kotlin/com/devtiro/bookstore/domain/author.kt
+++ src/main/kotlin/com/devtiro/bookstore/domain/author.kt
@@ -1,3 +1,6 @@
 package com.devtiro.bookstore.domain
 
+import jakarta.persistence.Entity
+
+@Entity
 data class Author (val id: Long?, val name: String, val age: Int, val description: String, val image: String)
