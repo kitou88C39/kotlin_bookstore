6a8e43a47982730c77a5855982fffb4ec0979736
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-04 14:33:45 +0900

    domain/entities/AuthorEntity.ktを編集する

---
+++ src/main/kotlin/com/devtiro/bookstore/domain/entities/AuthorEntity.kt
@@ -0,0 +1,27 @@
+package com.devtiro.bookstore.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name="author")
+@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_id_seq")
+data class Author (
+    @Id
+    @Column(name="id")
+    val id: Long?,
+
+    @Column(name="name")
+    val name: String,
+
+    @Column(name="age")
+    val age: Int,
+
+    @Column(name="description")
+    val description: String,
+
+    @Column(name="image")
+    val image: String)
