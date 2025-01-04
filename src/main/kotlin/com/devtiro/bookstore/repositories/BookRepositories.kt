ce7791defdf722e49d7a40d268ec024ac70f4d2e
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-04 10:09:16 +0900

    repositories/BookRepositories.ktを作成する

---
+++ src/main/kotlin/com/devtiro/bookstore/repositories/BookRepositories.kt
@@ -0,0 +1,10 @@
+package com.devtiro.bookstore.repositories
+
+import com.devtiro.bookstore.domain.Author
+import org.springframework.data.jpa.repositories.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface BookRepository : JpaRepository<Author, Long?>{
+
+}
