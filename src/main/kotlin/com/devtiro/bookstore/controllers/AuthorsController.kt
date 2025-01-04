2851668b7ff4a458e863f36a5132dcebebe4d188
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-04 12:24:36 +0900

    controllers/AuthorsController.ktを作成する

--- src/main/kotlin/com/devtiro/bookstore/controllers/AuthorsController.kt
+++ src/main/kotlin/com/devtiro/bookstore/controllers/AuthorsController.kt
@@ -1,13 +1,14 @@
 package com.devtiro.bookstore.controllers
 
 import org.springframework.web.bind.annoatation.PostMapping
+import org.springframework.web.bind.annoatation.RequestBody
 import org.springframework.web.bind.annoatation.RestController
 
 @RestController
 class AuthorController {
 
     @PostMapping(path = ["/v1/authors"])
-    fun createAuthor(){
+    fun createAuthor(@RequestBody author: Author){
 
     }
 }
