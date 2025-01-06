0e3882494f71d072340181bfc74796c24ecefc09
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-06 20:41:55 +0900

    domain/dto/BookDto.ktを作成する

--- src/main/kotlin/com/devtiro/bookstore/domain/dto/BookDto.kt
+++ src/main/kotlin/com/devtiro/bookstore/domain/dto/BookDto.kt
@@ -1,3 +1,9 @@
 package com.devtiro.bookstore.domain.dto
 
-data class BookDto()
+data class BookDto(
+    var isbn: String,
+    var title: String,
+    var description: String,
+    var image: String,
+    var author: Author
+)
