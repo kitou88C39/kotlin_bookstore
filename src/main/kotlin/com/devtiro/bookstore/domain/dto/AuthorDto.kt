3e071515470937aa1840b702c0dcc56e27c37fef
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-06 20:33:58 +0900

    domain/dto/AuthorDto.ktを作成する

--- src/main/kotlin/com/devtiro/bookstore/domain/dto/AuthorDto.kt
+++ src/main/kotlin/com/devtiro/bookstore/domain/dto/AuthorDto.kt
@@ -1,18 +1,8 @@
 package com.devtiro.bookstore.domain.dto
 
 data class AuthorDto(
-    @Id
-    @Column(name="id")
     val id: Long?,
-
-    @Column(name="name")
     val name: String,
-
-    @Column(name="age")
     val age: Int,
-
-    @Column(name="description")
     val description: String,
-
-    @Column(name="image")
     val image: String)
