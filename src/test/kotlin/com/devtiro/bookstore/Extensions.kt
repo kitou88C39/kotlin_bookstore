9f4ea72a2ae9caa9d5aac790cc42724ec282bfc1
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-07 21:35:14 +0900

    testkotlin/com/devtiro／bokkstore／Extensions.ktを作成する

--- src/test/kotlin/com/devtiro/bookstore/Extensions.kt
+++ src/test/kotlin/com/devtiro/bookstore/Extensions.kt
@@ -3,12 +3,18 @@ package com.devtiro.bookstore
 import com.devtiro.bookstore.domain.dto.AuthorDto
 import com.devtiro.bookstore.domain.entities.AuthorEntity
 
-fun AuthorEntity.toAuthorDto(): AuthorDto{
-    return AuthorDto(
+fun AuthorEntity.toAuthorDto() = AuthorDto(
     id=this.id,
     name=this.name,
     age=this.age,
     description=this.description,
     image=this.image
     )
-}
+
+    fun AuthorDto.toAuthorEntity() = AuthorEntity(
+    id=this.id,
+    name=this.name,
+    age=this.age,
+    description=this.description,
+    image=this.image
+    )
