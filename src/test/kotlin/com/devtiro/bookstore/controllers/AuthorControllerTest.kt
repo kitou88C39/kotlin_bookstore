ce1da6bd36f7257b5fd7903346d0fa0f003dfa0d
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-09 20:10:20 +0900

    test/kotlin/controllers/AuthorControllerTest.kt

--- src/test/kotlin/com/devtiro/bookstore/controllers/AuthorControllerTest.kt
+++ src/test/kotlin/com/devtiro/bookstore/controllers/AuthorControllerTest.kt
@@ -1,15 +1,19 @@
 package com.devtiro.bookstore.controllers
 
+import org.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.AutoConfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.post
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class AuthorControllerTest @Autowired constructor(val mockMvc: MockMvc){
+class AuthorControllerTest @Autowired constructor(private mockMvc: MockMvc){
 
     @Test
     fun `test that create Author returns a HTTP 201 status on a successful create`(){
-        a
+        mockMvc.post("/v1/authors")
     }
 
 }
