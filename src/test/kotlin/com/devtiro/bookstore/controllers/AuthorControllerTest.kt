f0a2f889f958f7ed63ba6c856eff5d74996a63d2
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-09 21:35:31 +0900

    test/kotlin/services/impl/AuthorServiceImpl.kt作成する

--- src/test/kotlin/com/devtiro/bookstore/controllers/AuthorControllerTest.kt
+++ src/test/kotlin/com/devtiro/bookstore/controllers/AuthorControllerTest.kt
@@ -62,4 +62,3 @@ class AuthorControllerTest @Autowired constructor(
         }
     }
 }
-    
---
+++ src/test/kotlin/services/impl/AuthorServiceImplTest.kt
@@ -0,0 +1,6 @@
+package com.devtiro.bookstore.services.impl
+
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthorServiceImplTest{}
