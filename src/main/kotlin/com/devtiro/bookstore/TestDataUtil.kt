2a938c192a1b7d381887118e2f0f07db75c92bc9
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-09 21:14:46 +0900

    kotlin/com/devtiro／bookstore／TestDataUtil.ktを作成する

--- src/main/kotlin/com/devtiro/bookstore/TestDataUtil.kt
+++ src/main/kotlin/com/devtiro/bookstore/TestDataUtil.kt
@@ -1,3 +1,9 @@
 package com.devtiro.bookstore
 
-fun testAuthorDtoA(id: Long? = null){}
+fun testAuthorDtoA(id: Long? = null) = AuthorDto(
+    id = id,
+    name = "John Doe",
+    age = 30,
+    description = "author-image.jpeg",
+    image = "some-description.jpeg"
+    )
