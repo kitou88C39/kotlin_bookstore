8b65324108b01f17a282d05aae8c6560d829f8a2
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-09 21:40:26 +0900

    test/kotlin/services/impl/AuthorServiceImpl.kt作成する

--- src/test/kotlin/services/impl/AuthorServiceImplTest.kt
+++ src/test/kotlin/services/impl/AuthorServiceImplTest.kt
@@ -1,9 +1,13 @@
 package com.devtiro.bookstore.services.impl
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annoatation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class AuthorServiceImplTest @Autowired constructor(private underTest: AuthorServiceImpl){
     @Test
+    fun `test that save persists the Author in the database`(){
+        
+    }
 }
