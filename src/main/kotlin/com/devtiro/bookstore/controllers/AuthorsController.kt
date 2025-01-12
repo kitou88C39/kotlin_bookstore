1442507ed5ae96a405c04aafa53de6970cdd9a76
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-12 11:41:48 +0900

    controllers/AuthorsController.ktを編集する

--- src/main/kotlin/com/devtiro/bookstore/controllers/AuthorsController.kt
+++ src/main/kotlin/com/devtiro/bookstore/controllers/AuthorsController.kt
@@ -17,14 +17,14 @@ class AuthorController(private val authorService: AuthorService) {
         ).toAtuthorDto()
         return ResponseEntity(createAuthor, HttpStatus.CREATED)
     }
-    
+
     @GetMapping
     fun readManyAuthor(): List<AuthorDto>{
         return authorService.list().map{it.toAtuthorDto()}
     }
 
-    @GetMapping
-    fun readManyAuthor(): List<AuthorDto>{
+    @GetMapping(path = ["/{id}"])
+    fun readOneAuthor(@PathVariable("id") id: Long): ResponseEntity<AuthorDto>{
         return authorService.list().map{it.toAtuthorDto()}
     }
 }
