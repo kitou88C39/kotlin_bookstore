d9eafdba89ad4a946c33951eb70fe92f29486cb1
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-13 16:54:43 +0900

    test/kotlin/services/impl/AuthorServiceImpl.ktを編集する

--- src/test/kotlin/services/impl/AuthorServiceImplTest.kt
+++ src/test/kotlin/services/impl/AuthorServiceImplTest.kt
@@ -8,38 +8,40 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class AuthorServiceImplTest @Autowired constructor(
-    private underTest: AuthorServiceImpl,
-    private val authorRepository: authorRepository){
+   private underTest: AuthorServiceImpl,
+   private val authorRepository: authorRepository){
 
-    @Test
-    fun `test that save persists the Author in the database`(){
-       val saveAuthor = underTest.save(testAuthorEntityA())
-       assertThat(saveAuthor.id).isNotNull()
+   @Test
+   fun `test that save persists the Author in the database`(){
+      val saveAuthor = underTest.save(testAuthorEntityA())
+      assertThat(saveAuthor.id).isNotNull()
 
-       val recalledAuthor = authorRepository.findById(saveAuthor.id)
-       assertThat(recalledAuthor).isNotNull()
-       assertThat(saveAuthor.id).isEqualTo(
-        testAuthorEntityA(id=saveAuthor.id)
-       )
-    }
+      val recalledAuthor = authorRepository.findById(saveAuthor.id)
+      assertThat(recalledAuthor).isNotNull()
+      assertThat(saveAuthor.id).isEqualTo(
+      testAuthorEntityA(id=saveAuthor.id)
+      )
+   }
 
-    @Test
-    fun `test that list returns empty list when no author in the database`(){
-       val result = underTest.list(testAuthorEntityA())
-       assertThat(result).isEmpty()
-    }
+   @Test
+   fun `test that list returns empty list when no author in the database`(){
+      val result = underTest.list(testAuthorEntityA())
+      assertThat(result).isEmpty()
+   }
 
-    @Test
-    fun `test that list returns authors when present in the database`(){
-       val saveAuthor = authorRepository.save(testAuthorEntityA())
-       val expected = ListOf(saveAuthor)
-       val result = underTest.list()
-       assertThat(result).isEqualTo(expected)
-    }
+   @Test
+   fun `test that list returns authors when present in the database`(){
+      val saveAuthor = authorRepository.save(testAuthorEntityA())
+      val expected = ListOf(saveAuthor)
+      val result = underTest.list()
+      assertThat(result).isEqualTo(expected)
+   }
 
-    @Test
-    fun `test that get returns null when author not present in the database`(){
+   @Test
+   fun `test that get returns null when author not present in the database`(){
       val result = underTest.get(999)
-      assertThat(result).isNotNull()
-    }
+      assertThat(result).isNull()
+   }
+
+
 }
