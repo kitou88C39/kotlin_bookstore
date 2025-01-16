481554d2c5853b7fa0a302c7a067040cfe37e118
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-16 21:39:15 +0900

    test/kotlin/services/impl/TestDataUtil.ktを作成する

--- src/test/kotlin/TestDataUtil.kt
+++ src/test/kotlin/TestDataUtil.kt
@@ -5,3 +5,11 @@ fun testAuthorEntityA(id: Long? = null) = AuthorEntity(
     description = "some description"
     image = "author-image.jpeg",
 )
+
+fun testAuthorEntityA(id: Long? = null) = AuthorEntity(
+    id = null,
+    name = "John Doe",
+    age = 30,
+    description = "some description"
+    image = "author-image.jpeg",
+)
