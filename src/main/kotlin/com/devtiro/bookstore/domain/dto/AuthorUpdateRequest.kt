3bf1b9f13685b32dfcbcc8ad143b9f172f9a6836
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-18 16:06:57 +0900

    domain/dto/AuthorUpdateRequest.ktを作成する

--- src/main/kotlin/com/devtiro/bookstore/domain/dto/AuthorUpdateRequest.kt
+++ src/main/kotlin/com/devtiro/bookstore/domain/dto/AuthorUpdateRequest.kt
@@ -2,8 +2,8 @@ package com.devtiro.bookstore.domain.dto
 
 data class AuthorUpdateRequest(
     val id: Long?,
-    val name: String,
-    val age: Int,
-    val description: String,
+    val name: String?,
+    val age: Int?,
+    val description: String?,
     val image: String
     )
