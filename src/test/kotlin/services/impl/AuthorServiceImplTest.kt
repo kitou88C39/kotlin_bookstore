a5a7054184d3a66a30dc5132daf1656d9e7b223d
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-20 20:57:55 +0900

    test/kotlin/services/impl/AuthorServiceImpl.ktを編集する

--- src/test/kotlin/services/impl/AuthorServiceImplTest.kt
+++ src/test/kotlin/services/impl/AuthorServiceImplTest.kt
@@ -154,3 +154,20 @@ fun `test that partial update Author updates author age`(){
          authorUpdateRequest = authorUpdateRequest,
       )
 }
+
+@Test
+fun `test that partial update Author updates author age`(){
+   val newDescription = "A new description"
+   val existingAuthor = existingAuthorEntityA()
+   val expectedAuthor = existingAuthor.copy(
+         description = newAge
+      )
+      val AuthorUpdateRequest = authorUpdateRequest(
+         description = newDescription
+      )
+      assertThatAuthorPartialUpdateIsUpdaed(
+         existingAuthor = existingAuthor,
+         expectedAuthor = expectedAuthor,
+         authorUpdateRequest = authorUpdateRequest,
+      )
+}
