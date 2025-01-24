3f07aa412ecdf264c17dff9341dcce20e41b152b
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-25 07:16:29 +0900

    domain/dto/AuthorSummeryDto.ktを作成する

--- src/main/kotlin/com/devtiro/bookstore/domain/dto/AuthorSummeryDto.kt
+++
@@ -1,9 +0,0 @@
-package com.devtiro.bookstore.domain.dto
-
-data class AuthorSummeryDto(
-    val id: Long?,
-    val name: String,
-    val age: Int,
-    val description: String,
-    val image: String
-    )
