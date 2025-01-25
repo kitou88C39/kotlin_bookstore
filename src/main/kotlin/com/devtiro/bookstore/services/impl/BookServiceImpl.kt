526827ed0328be72b597464faae8cee977e6958a
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-25 16:25:18 +0900

    test/kotlin/services/impl/BookServiceImpl.ktを作成する

--- src/main/kotlin/com/devtiro/bookstore/services/impl/BookServiceImpl.kt
+++ src/main/kotlin/com/devtiro/bookstore/services/impl/BookServiceImpl.kt
@@ -4,10 +4,14 @@ import com.devtiro.bookstore.domain.BookSummary
 import com.devtiro.bookstore.domain.entities.BookEntity
 import com.devtiro.bookstore.services.BookService
 
-class BookServiceImpl(val bookRepository: bookRepository) :BookService {
+class BookServiceImpl(
+    val bookRepository: bookRepository,
+    val authorRepository: AuthorRepository,) :BookService {
 
     override fun createUpdate(isbn: String, bookSummary: BookSummary): Pair<BookEntity, Boolean> {
         val normalisedBook = bookSummary.copy(isbn = isbn)
+        val isExists = bookRepository.existsById(isbn)
+        authorRepository.findByIdOrNull(normalisedBook.author.id)
     }
 
 }
