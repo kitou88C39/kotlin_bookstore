67f0d2cd9256cc1cadd20df27767afa68852dd37
kitoumasaomi <kanfutrooper@gmail.com>
2025-01-26 13:47:31 +0900

    test/kotlin/controllers/BooksControllerTest.ktを作成する

--- src/test/kotlin/com/devtiro/bookstore/controllers/BooksControllerTest.kt
+++ src/test/kotlin/com/devtiro/bookstore/controllers/BooksControllerTest.kt
@@ -1,5 +1,10 @@
 package com.devtiro.bookstore.controllers
 
+import com.devtiro.bookstore.services.AuthorService
+import com.ninjasquad.springmockk.MockBean
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.web.servlet.MockMvc
+
 class BooksControllerTest @Autowired constructor(
     private val mockMvc: MockMvc,
     @MockBean val authorService: AuthorService
