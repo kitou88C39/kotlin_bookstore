5850ff96679357e6be26b9836f5674446c87adde
kitoumasaomi <kanfutrooper@gmail.com>
2025-02-01 11:53:26 +0900

    ③services/impl/BookServiceImpl.ktを作成する

--- src/main/kotlin/com/devtiro/bookstore/repositories/BookRepository.kt
+++ src/main/kotlin/com/devtiro/bookstore/repositories/BookRepository.kt
@@ -1,11 +1,11 @@
 package com.devtiro.bookstore.repositories
 
-import com.devtiro.bookstore.domain.Author
+import com.devtiro.bookstore.domain.BookEntity
 import org.springframework.data.jpa.repositories.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface BookRepository : JpaRepository<Book, Long?> {
-    
-    fun findByAuthorEntity(id: Long): List<BookEntity>
+
+    fun findByAuthorEntityId(id: Long): List<BookEntity>
 }
