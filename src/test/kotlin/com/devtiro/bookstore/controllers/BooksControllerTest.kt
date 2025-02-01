71a0593cf9dd72a8d15dcd8f597336053eeceb43
kitoumasaomi <kanfutrooper@gmail.com>
2025-02-01 09:54:43 +0900

    test/kotlin/controllers/BooksControllerTest.ktを作成する

--- src/test/kotlin/com/devtiro/bookstore/controllers/BooksControllerTest.kt
+++ src/test/kotlin/com/devtiro/bookstore/controllers/BooksControllerTest.kt
@@ -119,7 +119,8 @@ class BooksControllerTest @Autowired constructor　(
             content { jsonPath ( "$[0].title", equalTo(isbn))}
             content { jsonPath ( "$[0].image", equalTo("book-image.jpeg"))}
             content { jsonPath ( "$[0].author.id", equalTo(1))}
-            content { jsonPath ( "$[0].author.id", equalTo(1))}
+            content { jsonPath ( "$[0].author.name", equalTo("John Doe"))}
+            content { jsonPath ( "$[0].author.image", equalTo("author-image.jpeg"))}
         }
     }
 }
