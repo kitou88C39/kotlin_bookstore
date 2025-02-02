a433225800e17867a9f56ca3118640f186eb2714
kitoumasaomi <kanfutrooper@gmail.com>
2025-02-02 16:50:10 +0900

    domain/dto/BookUpdateRequestDto.ktを作成する

--- src/main/kotlin/com/devtiro/bookstore/domain/dto/BookUpdateRequestDto.kt
+++ src/main/kotlin/com/devtiro/bookstore/domain/dto/BookUpdateRequestDto.kt
@@ -1,9 +1,7 @@
 package com.devtiro.bookstore.domain.dto
 
 data class BookUpdateRequestDto(
-    var isbn: String,
-    var title: String,
-    var description: String,
-    var image: String,
-    var author: AuthorSummaryDto
+    var title: String?,
+    var description: String?,
+    var image: String?,
 )
