5272771b71de7113d98bb96ff3a0d44aebf64458
kitoumasaomi <kanfutrooper@gmail.com>
2025-02-04 21:30:27 +0900

    test/kotlin/controllers/BooksControllerTest.ktを編集する

--- src/test/kotlin/com/devtiro/bookstore/controllers/BooksControllerTest.kt
+++ src/test/kotlin/com/devtiro/bookstore/controllers/BooksControllerTest.kt
@@ -228,10 +228,12 @@ class BooksControllerTest @Autowired constructor(
             bookService.partialUpdate(BOOK_A_ISBN, bookUpdateRequest)
         } throws IllegalStateException()
 
-        mockMvc.get("/v1/books/$BOOK_A_ISBN") {
+        mockMvc.patch("/v1/books/$BOOK_A_ISBN") {
             contentType = MediaType.APPLICATION_JSON
             accept = MediaType.APPLICATION_JSON
             content = objectMapper.writeValueAsString(bookUpdateRequestDto)
+        }.andExpect {
+            status { isBadRequest()}
         }
     }
 }
